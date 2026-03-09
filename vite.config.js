8ddf32e7362889e53d5b0d1652918ff2203dee6c
caiyiqiu <caiyiqiu2019@gmail.com>
2026-03-09 09:49:51 +0800

    fix(deploy): allow all hosts for preview to bypass vite security block

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // 监听所有以太网卡，以支持容器部署
     port: parseInt(process.env.PORT) || 8080,
+    allowedHosts: true // 允许所有外部域名访问 (Railway)
   },
   preview: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 8080,
+    allowedHosts: true // 允许所有外部域名访问 (Railway)
   }
 });
