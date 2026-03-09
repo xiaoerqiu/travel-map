c2c6f5f26cf4c450e74029419b041affba2ec182
caiyiqiu <caiyiqiu2019@gmail.com>
2026-03-09 09:44:38 +0800

    fix(deploy): force preview port to 8080 or pass from process.env.PORT

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: '0.0.0.0', // 监听所有以太网卡，以支持容器部署
-    port: process.env.PORT || 3000,
+    port: parseInt(process.env.PORT) || 8080,
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 3000,
+    port: parseInt(process.env.PORT) || 8080,
   }
 });
